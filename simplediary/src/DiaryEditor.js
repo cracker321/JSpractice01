832a352231301de8f039dc3eef2453ed66d5981c
unknown <joe@thekary.com>
2023-04-17 18:39:33 +0900

    yujong

---
+++ simplediary/src/DiaryEditor.js
@@ -0,0 +1,16 @@
+const DiaryEditor = () =>{
+
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input/>
+            </div>
+
+
+
+
+    </div>
+}
+
+export default DiaryEditor;
